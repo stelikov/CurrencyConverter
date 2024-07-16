d05cd6cece8937c4b0e24ec0a3e6b548353bfa9f
tumkast <vadste@gmai.com>
2024-07-16 22:56:02 +0300

    add config, add styles, add model

---
+++ CurrencyConverterApp.Server/Models/CurrencyApiResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace CurrencyConverterApp.Server.Models;
+
+public class CurrencyApiResponse
+{
+    public Dictionary<string, decimal> Rates { get; set; }
+    public DateTime Date { get; set; }
+}
